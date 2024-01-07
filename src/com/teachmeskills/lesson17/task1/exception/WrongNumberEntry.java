5380a430f4f8ee94b58a5a470837df06271f6cac
Shifernik <m.Disai@mail.ru>
2024-01-07 18:58:14 +0300

    Lesson 17
    Task 1
    Create a generalized functional interface.
    Write a class with one method.
    In this method, implement the logic:
    - if the number 1 is entered into the console, then: use an interface with a string type and pass the line reversal logic (outputting the line in reverse order) to the interface method.
    
    - if the number 2 is entered into the console, then: use an interface with an integer type and pass the logic for finding the factorial of the number to the interface method.
    
    Task 2 (optional)
    Get day of week by date

--- src/com/teachmeskills/lesson17/task1/exception/WrongNumberEntry.java
+++
@@ -1,9 +0,0 @@
-package com.teachmeskills.lesson17.task1.exception;
-
-public class WrongNumberEntry extends Exception {
-
-    public WrongNumberEntry(String wrong) {
-        super(wrong);
-    }
-
-}
